1c7b5166321542928b4d98f24b797c0f9d5cda5b
Mbvene <Shobatsu@gmail.com>
2021-04-23 04:20:34 +0300

    6 задание

--- Homework1/jogging.py
+++ Homework1/jogging.py
@@ -5,3 +5,4 @@ while last_distance > distance:
     distance = distance + (distance / 10)
     day = day + 1
 print(f"На {day}-й день спортсмен достиг результата — не менее {int(last_distance)} км.")
+

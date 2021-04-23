38056efaa4967362c50b1195f20d45c3170df15a
Mbvene <Shobatsu@gmail.com>
2021-04-23 04:18:33 +0300

    2 задание

--- Homework1/Sec_in_hour.py
+++ Homework1/Sec_in_hour.py
@@ -6,4 +6,3 @@ m = minutes % 60
 seconds = int(m % 60)
 # print("{}:{}:{}".format('%02d' % hours, '%02d' % minutes, '%02d' % seconds))
 print(f"{hours:02d}:{minutes:02d}:{seconds:02d}")
-# перевод секунд в чч:мм:сс
